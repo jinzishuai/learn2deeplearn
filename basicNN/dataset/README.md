217fe11e375b9c86018428b7da3ec4360275779a
Shi Jin <Shi.Jin@ctl.io>
2016-09-02 21:06:51 -0600

    add dataset: generation and data

---
+++ basicNN/dataset/README.md
@@ -0,0 +1,14 @@
+# Data Generation using scikit-learn
+
+## Installation of scikit-learn
+ref: http://scikit-learn.org/stable/install.html
+
+On Ubuntu
+```
+sudo apt-get install python-sklearn
+```
+
+# Data Visualization with matplotlib
+ref: http://matplotlib.org/
+
+
---
+++ basicNN/dataset/dataGen.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import numpy as np
+from sklearn import datasets, linear_model
+import matplotlib.pyplot as plt
+
+# Generate a dataset and plot it
+np.random.seed(0) #this data will be predictable, ie, always the same
+X, y = datasets.make_moons(200, noise=0.20)
+result = np.concatenate((X,y[:,None]),axis=1)
+np.savetxt('result.dat', result, fmt='%f %f %d', delimiter='\n')
+
+#visualize
+plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
+plt.show()
