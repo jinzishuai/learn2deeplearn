722b4e456b4ed734d4a0ecaffb7a4a2ba16fcf27
Shi Jin <Shi.Jin@ctl.io>
2016-09-06 20:10:13 -0600

    implement softmax function

--- basicNN/src/softmax.m
+++ basicNN/src/softmax.m
@@ -1,3 +1,7 @@
+#ref: https://en.wikipedia.org/wiki/Softmax_function
 function y = softmax(x)
-    y = x;
+    expX=exp(x);
+    len=length(x);
+    sumExpX=sum(expX,1);
+    y = expX./repmat(sumExpX,len,1);
 end
