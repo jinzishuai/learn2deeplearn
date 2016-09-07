f53359414d75ca97e9056068a02622813da143a8
Shi Jin <Shi.Jin@ctl.io>
2016-09-06 23:08:06 -0600

    understanding how the output vector is formed and how to calculate the error

--- basicNN/src/nn.m
+++ basicNN/src/nn.m
@@ -3,6 +3,8 @@ clc;
 #Load the data
 result=load('../dataset/result.dat');
 X=result(:,1:2);
+y=result(:,3); #Scalar form of the output
+yy=[1-y,y]; #Vector form of the output
 
 #Define constants
 num_examples = length(X); # training set size
@@ -18,17 +20,21 @@ nn_hdim = 3;
 num_passes = 1; #20000;
 
 #Initialize parameters to random numbers
-W1=randn(nn_input_dim, nn_hdim)/ sqrt(nn_input_dim);
+W1=randn(nn_input_dim, nn_hdim)/ sqrt(nn_input_dim); #dimension nn_input_dim x nn_hdim
 b1=zeros(1, nn_hdim);
-W2=randn(nn_hdim, nn_output_dim) /sqrt(nn_hdim);
+W2=randn(nn_hdim, nn_output_dim) /sqrt(nn_hdim); #dimension nn_hdim x nn_output_dim
 b2=zeros(1,nn_output_dim);
 
 for i=0:num_passes
     # Forward propagation
-    z1 = X*W1 + repmat(b1, num_examples,1); #we use repmat to apply the constant to all input values
-    a1 = tanh(z1);
-    z2 = a1*W2 + repmat(b2, num_examples,1);
-    a2 = softmax(z2);
+    z1 = X*W1 + repmat(b1, num_examples,1); #we use repmat to apply the constant to all input values, dimension num_examples x nn_hdim
+    a1 = tanh(z1); #dimension num_examples x nn_hdim
+    z2 = a1*W2 + repmat(b2, num_examples,1); #dimension num_examples x nn_output_dim
+    probs = softmax(z2);  #dimension num_examples x nn_output_dim
+    
+    # Backward progagation
+    delta3 = probs - yy; #dimension num_examples x nn_output_dim 
+    dW2 = a1'*delta3; #same dimension of W2: nn_hdim x nn_output_dim
     
 end 
 
