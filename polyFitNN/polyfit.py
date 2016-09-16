29d7579a282c4ffa3937f5e0b211c56d9a622715
Shi Jin <Shi.Jin@ctl.io>
2016-09-15 23:05:22 -0600

    code structure in place. currently got NAN

--- polyFitNN/polyfit.py
+++ polyFitNN/polyfit.py
@@ -11,55 +11,47 @@ class Config:
     # Gradient descent parameters (I picked these by hand)
     epsilon = 0.01  # learning rate for gradient descent
     reg_lambda = 0.01  # regularization strength
+    maxX=5;
 
 def func(x):
-    return x^3
+    return x**3
     
 def generate_data():
     np.random.seed(0)
-    x = np.random.rand(100)*10-5;
+    x = Config.maxX*(np.random.rand(10,1)*2-1);
     y = func(x)
     return x, y
 
 
 def visualize(x, y, model):
     # plt.scatter(X[:, 0], X[:, 1], s=40, c=y, cmap=plt.cm.Spectral)
-    # plt.show()
+    plt.plot(x,y,'ro')
+    sx = np.linspace(-Config.maxX, Config.maxX, num=20)
+    sy = func(sx)
+    plt.plot(sx,sy)
+    print model
+    px = np.matrix(sx).T
+    py = predict(model,px) *100
+    plt.plot(px, py, 'r-')
     plt.title("Function Fit")
-
-
-def plot_decision_boundary(pred_func, X, y):
-    # Set min and max values and give it some padding
-    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
-    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
-    h = 0.01
-    # Generate a grid of points with distance h between them
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-    # Predict the function value for the whole gid
-    Z = pred_func(np.c_[xx.ravel(), yy.ravel()])
-    Z = Z.reshape(xx.shape)
-    # Plot the contour and training examples
-    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
-    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral)
     plt.show()
 
 
+
 # Helper function to evaluate the total loss on the dataset
-def calculate_loss(model, X, y):
-    num_examples = len(X)  # training set size
+def calculate_loss(model, x, y):
+    num_examples = len(x)  # training set size
     W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
     # Forward propagation to calculate our predictions
-    z1 = X.dot(W1) + b1
+    z1 = x.dot(W1) + b1
     a1 = np.tanh(z1)
     z2 = a1.dot(W2) + b2
-    exp_scores = np.exp(z2)
-    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+  
     # Calculating the loss
-    corect_logprobs = -np.log(probs[range(num_examples), y])
-    data_loss = np.sum(corect_logprobs)
+    data_loss =0.5* np.sum(np.square(y-z2))
     # Add regulatization term to loss (optional)
     data_loss += Config.reg_lambda / 2 * (np.sum(np.square(W1)) + np.sum(np.square(W2)))
-    return 1. / num_examples * data_loss
+    return 1.0/ num_examples * data_loss
 
 
 def predict(model, x):
@@ -68,18 +60,16 @@ def predict(model, x):
     z1 = x.dot(W1) + b1
     a1 = np.tanh(z1)
     z2 = a1.dot(W2) + b2
-    exp_scores = np.exp(z2)
-    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
-    return np.argmax(probs, axis=1)
+    return z2
 
 
 # This function learns parameters for the neural network and returns the model.
 # - nn_hdim: Number of nodes in the hidden layer
 # - num_passes: Number of passes through the training data for gradient descent
 # - print_loss: If True, print the loss every 1000 iterations
-def build_model(X, y, nn_hdim, num_passes=20000, print_loss=False):
+def build_model(x, y, nn_hdim, num_passes=10000, print_loss=False):
     # Initialize the parameters to random values. We need to learn these.
-    num_examples = len(X)
+    num_examples = len(x)
     np.random.seed(0)
     W1 = np.random.randn(Config.nn_input_dim, nn_hdim) / np.sqrt(Config.nn_input_dim)
     b1 = np.zeros((1, nn_hdim))
@@ -93,19 +83,16 @@ def build_model(X, y, nn_hdim, num_passes=20000, print_loss=False):
     for i in range(0, num_passes):
 
         # Forward propagation
-        z1 = X.dot(W1) + b1
+        z1 = x.dot(W1) + b1
         a1 = np.tanh(z1)
         z2 = a1.dot(W2) + b2
-        exp_scores = np.exp(z2)
-        probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
-
+        
         # Backpropagation
-        delta3 = probs
-        delta3[range(num_examples), y] -= 1
+        delta3 = y - z2
         dW2 = (a1.T).dot(delta3)
         db2 = np.sum(delta3, axis=0, keepdims=True)
         delta2 = delta3.dot(W2.T) * (1 - np.power(a1, 2))
-        dW1 = np.dot(X.T, delta2)
+        dW1 = np.dot(x.T, delta2)
         db1 = np.sum(delta2, axis=0)
 
         # Add regularization terms (b1 and b2 don't have regularization terms)
@@ -124,7 +111,7 @@ def build_model(X, y, nn_hdim, num_passes=20000, print_loss=False):
         # Optionally print the loss.
         # This is expensive because it uses the whole dataset, so we don't want to do it too often.
         if print_loss and i % 1000 == 0:
-            print("Loss after iteration %i: %f" % (i, calculate_loss(model, X, y)))
+            print("Loss after iteration %i: %f" % (i, calculate_loss(model, x, y)))
 
     return model
 
@@ -138,9 +125,9 @@ def classify(X, y):
 
 
 def main():
-    X, y = generate_data()
-    model = build_model(X, y, 3, print_loss=True)
-    visualize(X, y, model)
+    x, y = generate_data()
+    model = build_model(x, y, 3, print_loss=True)
+    visualize(x, y, model)
 
 
 if __name__ == "__main__":
