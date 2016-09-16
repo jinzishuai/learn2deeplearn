8e330f22d1f25239012edc0c826836fea88eb26e
Shi Jin <Shi.Jin@ctl.io>
2016-09-15 23:41:55 -0600

    sort of working. Key: delta defined with direction

--- polyFitNN/polyfit.py
+++ polyFitNN/polyfit.py
@@ -10,15 +10,15 @@ class Config:
     nn_output_dim = 1  # output layer dimensionality
     # Gradient descent parameters (I picked these by hand)
     epsilon = 0.01  # learning rate for gradient descent
-    reg_lambda = 0.01  # regularization strength
-    maxX=5;
+    reg_lambda = 0.01 #0.01  # regularization strength
+    maxX=1;
 
 def func(x):
     return x**3
     
 def generate_data():
     np.random.seed(0)
-    x = Config.maxX*(np.random.rand(10,1)*2-1);
+    x = Config.maxX*(np.random.rand(100,1)*2-1);
     y = func(x)
     return x, y
 
@@ -26,12 +26,12 @@ def generate_data():
 def visualize(x, y, model):
     # plt.scatter(X[:, 0], X[:, 1], s=40, c=y, cmap=plt.cm.Spectral)
     plt.plot(x,y,'ro')
-    sx = np.linspace(-Config.maxX, Config.maxX, num=20)
+    sx = np.linspace(-Config.maxX, Config.maxX, num=1000)
     sy = func(sx)
     plt.plot(sx,sy)
-    print model
+    #print model
     px = np.matrix(sx).T
-    py = predict(model,px) *100
+    py = predict(model,px)
     plt.plot(px, py, 'r-')
     plt.title("Function Fit")
     plt.show()
@@ -88,7 +88,7 @@ def build_model(x, y, nn_hdim, num_passes=10000, print_loss=False):
         z2 = a1.dot(W2) + b2
         
         # Backpropagation
-        delta3 = y - z2
+        delta3 = z2 - y
         dW2 = (a1.T).dot(delta3)
         db2 = np.sum(delta3, axis=0, keepdims=True)
         delta2 = delta3.dot(W2.T) * (1 - np.power(a1, 2))
@@ -115,18 +115,9 @@ def build_model(x, y, nn_hdim, num_passes=10000, print_loss=False):
 
     return model
 
-
-def classify(X, y):
-    # clf = linear_model.LogisticRegressionCV()
-    # clf.fit(X, y)
-    # return clf
-
-    pass
-
-
 def main():
     x, y = generate_data()
-    model = build_model(x, y, 3, print_loss=True)
+    model = build_model(x, y, 3,num_passes=20000,  print_loss=True)
     visualize(x, y, model)
 
 
