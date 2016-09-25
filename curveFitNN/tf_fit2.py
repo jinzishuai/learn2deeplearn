6ed863a7259f93bb8b7aab67dbfca20666683e63
Shi Jin <Shi.Jin@ctl.io>
2016-09-24 19:46:49 -0600

    tf_fit2.py is now working properly in a similar way to the sample code of https://github.com/tensorflow/tensorflow/blob/r0.10/tensorflow/examples/tutorials/mnist/mnist_softmax.py

--- curveFitNN/tf_fit2.py
+++ curveFitNN/tf_fit2.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+sess = tf.InteractiveSession()
 
 ######################### Define the model ########################
 ## define the dimensions
@@ -12,8 +13,15 @@ nn_output_dim = 1
 nn_hdim = 1 #Hidden node dimension
 # Create 100 phony x, y data points in NumPy, y = x * 0.1 + 0.3
 num_examples = 100
-x = tf.Variable(tf.random_uniform([num_examples,nn_input_dim]))
-y_ = x**3 + x**2 + x
+
+x = tf.placeholder(tf.float32, [None, nn_input_dim])
+y_ = tf.placeholder(tf.float32, [None, nn_output_dim])
+
+x_data = np.random.rand(num_examples, nn_input_dim).astype(np.float32)
+y_data = x_data**3 + x_data**2 + x_data
+plt.plot(x_data,  y_data,  'ro')
+
+
 W1 = tf.Variable(tf.zeros([nn_input_dim, nn_hdim]))
 b1 = tf.Variable(tf.zeros([nn_hdim]))
 W2 = tf.Variable(tf.zeros( [nn_hdim, nn_output_dim]))
@@ -27,27 +35,22 @@ y = tf.matmul(a1, W2) + b2
 ## Define loss and optimizer
 # Minimize the mean squared errors.
 loss = tf.reduce_mean(tf.square(y - y_))
-optimizer = tf.train.GradientDescentOptimizer(0.001)
-train = optimizer.minimize(loss)
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 ######################### End of model definition ########################
 
-# Before starting, initialize the variables.  We will 'run' this first.
-init = tf.initialize_all_variables() 
 
-# Launch the graph.
-sess = tf.Session()
-sess.run(init)
+tf.initialize_all_variables().run()
+NSteps=10000
+NReport = NSteps/10
+for step in range(NSteps+1):
+    train_step.run({x:x_data,  y_:y_data})
+    if step % NReport == 0:
+        print(step, loss.eval({x:x_data,  y_:y_data}))
 
-plt.plot(x,  y_,  'ro')
+# # Test trained model
+#correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+#accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+#print(accuracy.eval({x: mnist.test.images, y_: mnist.test.labels})
+x_test = np.linspace(0.0,  1.0,  1000).reshape(1000, 1)
+plt.plot(x_test,  y.eval({x:x_test}) ,  'b-')
 plt.show()
-
-# Fit the line.
-for step in range(200001):
-    sess.run(train)
-    if step % 10000 == 0:
-        print(step, sess.run(W1), sess.run(b1), sess.run(W2), sess.run(b2),  sess.run(loss))
-    if step == 200000:
-        plt.plot(x,  y_,  'ro')
-        plt.plot(x,  sess.run(y),   'b*')
-        plt.show()
-
