26097fa290537521f35cc62936e8a54a1b72b48d
Shi Jin <Shi.Jin@ctl.io>
2017-10-15 23:11:52 -0600

    fixed week4ex2.py permissions but datasets needs redownload

--- deeplearning.ai/C1.NN_DL/week4/Deep Neural Network Application Image Classification/week4ex2.py
+++ deeplearning.ai/C1.NN_DL/week4/Deep Neural Network Application Image Classification/week4ex2.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/python3
 # coding: utf-8
 
 # # Deep Neural Network for Image Classification: Application
@@ -33,13 +33,13 @@ from PIL import Image
 from scipy import ndimage
 from dnn_app_utils_v2 import *
 
-get_ipython().magic('matplotlib inline')
+#get_ipython().magic('matplotlib inline')
 plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
-get_ipython().magic('load_ext autoreload')
-get_ipython().magic('autoreload 2')
+#get_ipython().magic('load_ext autoreload')
+#get_ipython().magic('autoreload 2')
 
 np.random.seed(1)
 
