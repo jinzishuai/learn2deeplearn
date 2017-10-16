52d8c9d69da968f11d093c9612c302b3395683b4
Shi Jin <Shi.Jin@ctl.io>
2017-10-15 22:27:00 -0600

    modified week4ex1.py that runs without ipython, directly in U16

--- deeplearning.ai/C1.NN_DL/week4/Building your Deep Neural Network - Step by Step/week4ex1.py
+++ deeplearning.ai/C1.NN_DL/week4/Building your Deep Neural Network - Step by Step/week4ex1.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/python3
 # coding: utf-8
 
 # # Building your Deep Neural Network: Step by Step
@@ -40,13 +40,13 @@ import matplotlib.pyplot as plt
 from testCases_v3 import *
 from dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
 
-get_ipython().magic('matplotlib inline')
+#get_ipython().magic('matplotlib inline')
 plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
-get_ipython().magic('load_ext autoreload')
-get_ipython().magic('autoreload 2')
+#get_ipython().magic('load_ext autoreload')
+#get_ipython().magic('autoreload 2')
 
 np.random.seed(1)
 
