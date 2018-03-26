664f95e7cfd21aea863b1feea3b7103f2e286360
Shi Jin <shi.jin@ctl.io>
2018-03-25 22:31:16 -0600

    solves problem 5 of lesson 1

--- google_dl_udacity/lesson1/problems/problem5.py
+++ google_dl_udacity/lesson1/problems/problem5.py
@@ -7,6 +7,10 @@ import data
 
 def look_for_duplicates (dataset):
     print ('looking for duplicates in a dataset of shape: ', dataset.shape)
+    sorted, indices = np.unique(dataset, axis=0, return_index = True)
+    print('after unique sort, the shape of dataset is ', sorted.shape)
+    print("there are %d duplicate entries out of %d, unique rate of %.2f%%" % ( (dataset.shape[0]-sorted.shape[0]), dataset.shape[0], sorted.shape[0]*100.0/dataset.shape[0]) )
+    return sorted, indices
 
 def sort4images(mytest):
     plt.figure(1)
@@ -36,12 +40,24 @@ def sort4images(mytest):
 letters=np.array(['A', 'B', 'C','D','E','F','G','H','I','J'])
 train_dataset, train_labels, valid_dataset, valid_labels, test_dataset, test_labels = data.load_all_data_from_single_pickle_file('../notMNIST.pickle')
 # sort the train_set
-print('my test results are',letters[train_labels[0:4]])
-sorted, indices = sort4images(train_dataset[0:4,:,:])
-print('my sorted test results are',letters[train_labels[indices]])
-plt.show()
+#print('my test results are',letters[train_labels[0:4]])
+#sorted, indices = sort4images(train_dataset[0:4,:,:])
+#print('my sorted test results are',letters[train_labels[indices]])
+#plt.show(block=False)
 
-#look_for_duplicates (valid_dataset)
+sorted, indices = look_for_duplicates (valid_dataset)
+uniqie_valid_dataset = valid_dataset[indices]
+unique_valid_labels = valid_labels[indices]
+
+sorted, indices = look_for_duplicates (test_dataset)
+uniqie_test_dataset = test_dataset[indices]
+unique_test_labels = test_labels[indices]
+
+sorted, indices = look_for_duplicates (train_dataset)
+uniqie_train_dataset = train_dataset[indices]
+unique_train_labels = train_labels[indices]
+
+look_for_duplicates (np.concatenate((train_dataset,valid_dataset,test_dataset)))
 
 #num_train_samples = train_dataset.shape[0]
 #print("dataset type is", type(train_dataset) )
